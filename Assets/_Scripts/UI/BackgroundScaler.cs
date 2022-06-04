c97a2c6a9bd401680efa8d4762c22f9cf43d75c8
Raphi666Metal <gaminghome@web.de>
2022-06-04 20:13:38 +0200

    First UI

---
+++ Assets/_Scripts/UI/BackgroundScaler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScaler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float height = Camera.main.orthographicSize * 2f;
+        float width = height * Screen.width / Screen.height;
+
+        transform.localScale = new Vector3(width, height, 0);
+    }
+}
